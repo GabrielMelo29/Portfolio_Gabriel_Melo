46bf837a019328946c69e940fad806d09cd48169
GabrielMelo29 <gbrlmelo29@gmail.com>
2023-04-27 14:00:10 -0300

    atualização no html css e js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const menu = document.querySelector(".header")
+const menu = document.querySelector(".site-header")
 
 function ativarScroll() {
     menu.classList.toggle('scrolly', scrollY > 20);
@@ -7,8 +7,9 @@ function ativarScroll() {
 window.addEventListener('scroll', ativarScroll);
 
 // -------- Abrir e fechar menu ---------
-const btnMenu = document.querySelector(".btn-menu");
-const listaMenu = document.querySelector(".list-menu");
+const btnMenu = document.querySelector(".menu-btn");
+const listaMenu = document.querySelector(".nav-links");
+const menuLists = document.querySelectorAll('.nav-links li');
 const btnIcon = document.querySelector(".btn-icon");
 
 function abrirMenu() {
@@ -17,3 +18,11 @@ function abrirMenu() {
 }
 
 btnMenu.addEventListener('click', abrirMenu);
+
+for (var listItem of menuLists) {
+    listItem.addEventListener("click", () => {
+  
+      listaMenu.classList.remove("active")
+      btnIcon.classList.remove('active')
+    }
+)}
